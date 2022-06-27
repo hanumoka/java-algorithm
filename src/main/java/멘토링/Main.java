15f583bb59e507249bcbe896bb05a2f1071633a3
amagramer <amagramer@gmail.com>
2022-06-27 23:38:08 +0900

    멘토링 아직 안품

---
+++ src/main/java/멘토링/Main.java
@@ -0,0 +1,16 @@
+package 멘토링;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println(solution());
+        }
+    }
+
+    public static String solution(){
+        return "1234";
+    }
+}
+
